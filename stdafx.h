3374478a4f4951c556426ae2892d5baf2a3d7f5e
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2014-07-11 14:18:48 +0900

    v0.6.3

--- stdafx.h
+++ stdafx.h
@@ -41,7 +41,7 @@ inline int MzcGetLParamY(LPARAM lParam)
 
 const char * const ch_logo = 
         "       +--------------------------------+\n"
-        "       |  ‚Ð‚ç‚ª‚È“d‘ì Calc-H ver.0.6.2 |\n"
+        "       |  ‚Ð‚ç‚ª‚È“d‘ì Calc-H ver.0.6.3 |\n"
         "       |   by •ÐŽR”Ž•¶MZ (katahiromz)   |\n"
         "       | http://katahiromz.web.fc2.com/ |\n"
         "       | katayama.hirofumi.mz@gmail.com |\n"
