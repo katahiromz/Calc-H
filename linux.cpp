0bd747da95a4364ff6c99e14d13e33a342013936
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-04-07 22:05:34 +0900

    support Linux

--- linux.cpp
+++ linux.cpp
@@ -5,6 +5,7 @@
 // (Japanese, UTF-8)
 
 #include "stdafx.h"
+#include <thread>
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -14,44 +15,111 @@ public:
     virtual ~MainWindow();
 
 protected:
-    virtual void on_button_clicked();
+    virtual void on_button_enter();
 
-    Gtk::VBox   m_vbox;
-    Gtk::Label  m_label;
-    Gtk::Button m_button;
+    Gtk::VBox       m_vbox;
+        Gtk::TextView   m_textview;
+        Gtk::HBox       m_hbox;
+            Gtk::Label      m_label;
+            Gtk::Entry      m_entry;
+            Gtk::Button     m_button_enter;
+
+    Glib::RefPtr<Gtk::TextBuffer> m_text_buf;
+
+    void add_output(const std::string& contents);
 }; // class MainWindow
 
 ////////////////////////////////////////////////////////////////////////////
 
 MainWindow::MainWindow() {
-    set_title("TypicalGtkmmApp");
-    set_size_request(300, 150);
+    set_title("ひらがな電卓Calc-H ver.0.8.8");
+    set_size_request(500, 280);
 
-    set_border_width(10);
+    Pango::FontDescription font_desc;
+    #ifdef _WIN32
+        font_desc.set_family("MS Gothic");
+    #else
+        font_desc.set_family("typewriter");
+    #endif
+    m_label.modify_font(font_desc);
+    m_entry.modify_font(font_desc);
+    m_textview.modify_font(font_desc);
+    m_button_enter.modify_font(font_desc);
 
-    m_label.set_text("Hello, world!");
+    set_border_width(4);
+    m_hbox.set_border_width(8);
+    m_vbox.set_border_width(8);
 
-    m_button.set_label("Click me!");
-    m_button.signal_clicked().connect(
-        sigc::mem_fun(*this, &MainWindow::on_button_clicked)
+    m_text_buf = Gtk::TextBuffer::create();
+    m_textview.set_buffer(m_text_buf);
+
+    m_label.set_label("にゅうりょく:");
+    m_entry.set_text("");
+
+    m_button_enter.set_label("Enter");
+    m_button_enter.signal_clicked().connect(
+        sigc::mem_fun(*this, &MainWindow::on_button_enter)
     );
 
-    m_vbox.pack_start(m_label);
-    m_vbox.pack_start(m_button, Gtk::PACK_SHRINK);
+    m_hbox.pack_start(m_label, Gtk::PACK_SHRINK);
+    m_hbox.pack_start(m_entry);
+    m_hbox.pack_start(m_button_enter, Gtk::PACK_SHRINK);
 
-    m_vbox.set_border_width(10);
+    m_vbox.pack_start(m_textview);
+    m_vbox.pack_start(m_hbox, Gtk::PACK_SHRINK);
+
     add(m_vbox);
 
     show_all_children();
+    m_button_enter.set_can_default();
+
+    set_focus(m_entry);
+
+    std::string contents;
+    contents += ch_logo;
+    contents += '\n';
+    add_output(contents);
 } // MainWindow::MainWindow
 
 MainWindow::~MainWindow() {
     ;
 } // MainWindow::~MainWindow
 
-void MainWindow::on_button_clicked() {
-    printf("Hello, world\n");
-    hide();
+void MainWindow::on_button_enter() {
+    std::thread(
+        [this](int dummy) {
+            std::string query = m_entry.get_text();
+
+            m_entry.set_sensitive(false);
+            m_entry.set_text("計算中です...");
+
+            CrTrimString(query);
+            auto result = ChJustDoIt(query);
+            ChFixResultForDisplay(result);
+
+            std::string contents("にゅうりょく：");
+            contents += query;
+            contents += '\n';
+            contents += result;
+            contents += '\n';
+            add_output(contents);
+
+            if (result.find("しゅうりょうします") != std::string::npos) {
+                m_entry.set_text("終了中です...");
+                hide();
+            }
+
+            m_entry.set_text("");
+            m_entry.set_sensitive(true);
+            set_focus(m_entry);
+        }, 0
+    ).detach();
+}
+
+void MainWindow::add_output(const std::string& contents) {
+    m_text_buf->set_text(m_text_buf->get_text() + contents);
+    auto end = m_text_buf->get_iter_at_line(m_text_buf->get_line_count() + 100);
+    m_textview.scroll_to(end);
 }
 
 ////////////////////////////////////////////////////////////////////////////
